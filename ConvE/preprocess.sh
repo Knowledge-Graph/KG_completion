f24bd8bb35e588faa149e7e6085b4c1579ac2ebc
fa <akrami.cs@gmail.com>
2020-12-18 19:57:28 -0600

    ConvE

--- ConvE/preprocess.sh
+++ ConvE/preprocess.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 mkdir data
-#mkdir data/WN18RR
-#mkdir data/YAGO3-10
-mkdir data/FB15k-237
-#mkdir data/nations
-#mkdir data/kinship
-#mkdir data/umls
-#mkdir saved_models
-#tar -xvf WN18RR.tar.gz -C data/WN18RR
-#tar -xvf YAGO3-10.tar.gz -C data/YAGO3-10
-tar -xvf FB15k-237.tar.gz -C data/FB15k-237
-#tar -xvf umls.tar.gz -C data/umls
-#tar -xvf kinship.tar.gz -C data/kinship
-#tar -xvf nations.tar.gz -C data/nations
-#python wrangle_KG.py WN18RR
-#python wrangle_KG.py YAGO3-10
+mkdir saved_models
+cp -r ../datasets/WN18RR ./data
+cp -r ../datasets/YAGO3-10 ./data
+cp -r ../datasets/FB15k-237 ./data
+cp -r ../datasets/FB15k ./data
+cp -r ../datasets/WN18 ./data
+cp -r ../datasets/YAGO3-10-DR ./data
+
+python wrangle_KG.py WN18RR
+python wrangle_KG.py WN18
+python wrangle_KG.py YAGO3-10
 python wrangle_KG.py FB15k-237
-#python wrangle_KG.py umls
-#python wrangle_KG.py nations
-#python wrangle_KG.py kinship
+python wrangle_KG.py FB15k
+python wrangle_KG.py YAGO3-10-DR
+
